4a87aa3926b12ae8806c0faf71aa7cf01826aecc
Rahul Sharma <rs3296472t@gmail.com>
2025-12-16 23:02:02 +0530

    sdsd

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function CourseCertificatePage() {
         // fixed: Pins it to the window (ignores scroll)
         // top-[45vh]: Places it 45% down from the top (50% is center, so 45% is slightly above)
         // left-1/2 -translate-x-1/2: Perfect horizontal centering
-        className="fixed cursor-pointer top-[35vh] left-1/2 -translate-x-1/2 bg-[#3070bf] text-white font-serif text-base py-2 px-6 rounded-md hover:bg-[#245894] transition-colors z-50"
+        className="fixed cursor-pointer top-[35vh] left-1/2 -translate-x-1/2 bg-[#3070bf] text-white font-serif text-base py-2 px-6 rounded-md  transition-colors z-50"
       >
         Course Certificate
       </button>
